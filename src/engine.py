de25d5a83e037f61b3865348a565ce3c95ecae38
Conor Hickey <conor.hickey1@gmail.com>
2026-06-25 17:58:30 +0100

    added ui and logging

--- src/engine.py
+++ src/engine.py
@@ -1,12 +1,16 @@
 from __future__ import annotations
 
 from typing import TYPE_CHECKING
-from tcod.context import Context
 from tcod.console import Console
 from tcod.map import compute_fov
 
 from src.game_map.game_map import GameMap
 from src.input_handlers import MainGameEventHandler
+from src.user_interface.render_functions import (
+    render_bar,
+    render_names_at_mouse_location,
+)
+from src.user_interface.message_log import MessageLog
 
 if TYPE_CHECKING:
     from src.entity_handlers.entity import Actor
@@ -19,7 +23,9 @@ class Engine:
 
     def __init__(self, player: Actor):
         self.event_handler: EventHandler = MainGameEventHandler(self)
+        self.mouse_location = (0, 0)
         self.player = player
+        self.message_log = MessageLog()
 
     def handle_enemy_turns(self) -> None:
         for entity in set(self.game_map.actors) - {self.player}:
@@ -36,15 +42,14 @@ class Engine:
         # If a tile is "visible" it should be added to "explored".
         self.game_map.explored |= self.game_map.visible
 
-    def render(self, console: Console, context: Context) -> None:
+    def render(self, console: Console) -> None:
         self.game_map.render(console)
+        self.message_log.render(console=console, x=21, y=45, width=40, height=5)
 
-        console.print(
-            x=1,
-            y=47,
-            string=f"HP: {self.player.fighter.hp}/{self.player.fighter.max_hp}",
+        render_bar(
+            console=console,
+            current_value=self.player.fighter.hp,
+            maximum_value=self.player.fighter.max_hp,
+            total_width=20,
         )
-
-        context.present(console)
-
-        console.clear()
+        render_names_at_mouse_location(console=console, engine=self)
